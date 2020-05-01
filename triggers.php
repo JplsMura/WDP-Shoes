3a5c7cd73e83b1d4044146c6723e680257d8bcf5
JplsMura <joaopedrolima.s@hotmail.com>
2020-05-01 11:06:13 -0300

    Project

---
+++ triggers.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>WDP: Avisos e Erros</title>
+    <link rel="stylesheet" href="assets/styles/Boot.css">
+    <link rel="stylesheet" href="assets/styles/Icons.css">
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700;800&display=swap');
+
+        *{
+            font-family: 'Open Sans', sans-serif;
+        }
+
+    </style>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="flex">
+            <div class="flex-2">
+                <!--QUADRADO-->
+                <h1>ALERT NORMAL</h1>
+                <div class="trigger trigger-sucess icon-sucess">Tudo Certo !</div>
+                <div class="trigger trigger-info icon-info">Veja isto !</div>
+                <div class="trigger trigger-alert icon-alert">Espera ai !</div>
+                <div class="trigger trigger-error icon-error">Opps, erro !</div>
+
+                <!--REDONDO-->
+                <h1>ALERT RADIUS</h1>
+                <div class="trigger trigger-sucess icon-sucess radius">Tudo Certo !</div>
+                <div class="trigger trigger-info icon-info radius">Veja isto !</div>
+                <div class="trigger trigger-alert icon-alert radius">Espera ai !</div>
+                <div class="trigger trigger-error icon-error radius">Opps, erro !</div>
+            </div>
+
+            <div class="flex-2">
+                <!--QUADRADO-->
+                <h1>ALERT NORMAL OUT</h1>
+                <div class="trigger trigger-sucess-out icon-sucess">Tudo Certo !</div>
+                <div class="trigger trigger-info-out icon-info">Veja isto !</div>
+                <div class="trigger trigger-alert-out icon-alert">Espera ai !</div>
+                <div class="trigger trigger-error-out icon-error">Opps, erro !</div>
+
+                <!--REDONDO-->
+                <h1>ALERT RADIUS OUT</h1>
+                <div class="trigger trigger-sucess-out icon-sucess radius">Tudo Certo !</div>
+                <div class="trigger trigger-info-out icon-info radius">Veja isto !</div>
+                <div class="trigger trigger-alert-out icon-alert radius">Espera ai !</div>
+                <div class="trigger trigger-error-out icon-error radius">Opps, erro !</div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
