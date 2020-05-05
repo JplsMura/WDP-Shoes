69b0260169ecfb6d568d3b359236d1f5073ca369
JplsMura <joaopedrolima.s@hotmail.com>
2020-05-05 07:37:02 -0300

    Tree commit

--- index.php
+++ index.php
@@ -5,10 +5,21 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta data-icon="">
+
     <title>Wdp Shoes | Sua Loja de ativos esportivos</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="Assets/Styles/Icons.css">
+    <link rel="stylesheet" href="Assets/Styles/Boot.css">
+    <link rel="stylesheet" href="Themes/WdpShoes/Style.css">
+    <link rel="shortcut icon" href="Themes/WdpShoes/images/favicon.png">
+
 </head>
 <body>
-    <h1>Teste nessa bagaça velha</h1>
+    <header class="main_header"></header>
+    <main class="main_content">
+        <h1>Olá mundo bora programar !!!</h1>
+    </main>
+    <footer class="main_footer"></footer>
 </body>
 </html>
