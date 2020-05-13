e92e28558a29c5ca72eab2eb9eaef38cbe762ef0
JplsMura <joaopedrolima.s@hotmail.com>
2020-05-13 08:18:19 -0300

    Estilização do Header Menu LOgo, Search, Categorias e Menu de Minha conta

--- index.php
+++ index.php
@@ -1,25 +1,75 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="mit" content="2020-04-20T07:03:21-03:00+171174">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>WdpShoes | Home</title>
 
-    <title>Wdp Shoes | Sua Loja de ativos esportivos</title>
-
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="Assets/Styles/Icons.css">
-    <link rel="stylesheet" href="Assets/Styles/Boot.css">
-    <link rel="stylesheet" href="Themes/WdpShoes/Style.css">
-    <link rel="shortcut icon" href="Themes/WdpShoes/images/favicon.png">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,800" rel="stylesheet">
+    <link rel="stylesheet" href="Assets/Styles/Icons.css"/>
+<!--    <link href="https://file.myfontastic.com/rAgByRoZ99iS7bHZ5ZXNwh/icons.css" rel="stylesheet">-->
+    <link rel="stylesheet" href="Assets/Styles/Boot.css"/>
+    <link rel="stylesheet" href="Themes/WdpShoes/Style.css"/>
+    <link rel="shortcut icon" href="Themes/WdpShoes/Images/favicon.png"/>
 
 </head>
 <body>
-    <header class="main_header"></header>
-    <main class="main_content">
-        <h1>Olá mundo bora programar !!!</h1>
-    </main>
-    <footer class="main_footer"></footer>
+<header class="main_header">
+    <div class="container">
+        <div class="main_header_nav">
+            <div class="main_header_nav_logo">
+                <a href="" title="WdpShoes | Home">
+                    <img alt="" title="" src="Themes/WdpShoes/Images/wdpshoes_logo_white.png"/>
+                </a>
+            </div>
+            <div class="main_header_nav_search">
+                <form action="" method="post" class="radius">
+                    <input type="text" name="s" placeholder="Pesquisar na WdpShoes:"/>
+                    <button class="icon-search icon-notext transition"></button>
+                </form>
+            </div>
+            <div class="main_header_nav_menu">
+                <a href="#" class="icon-cart icon-notext transition main_header_nav_menu_cart"><span>3</span></a>
+                <div class="main_header_nav_menu_user">
+                    <a href="#" title="" class="icon-user main_header_nav_menu_user_a radius transition">Minha Conta</a>
+                    <nav class="radius">
+                        <a href="#" title="">Meus pedidos</a>
+                        <a href="#" title="">Meus dados</a>
+                        <a href="#" title="">Meus endereços</a>
+                        <a href="#" title="">Sair</a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
+        <ul class="main_header_departments">
+            <?php
+            for ($i = 1; $i < 6; $i++) {
+                ?>
+                <li class="main_header_departments_li">Departamento <?= $i; ?>
+                    <ul class="main_header_departments_li_ul">
+                        <?php
+                        for ($ii = 1; $ii < 6; $ii++) {
+                            ?>
+                            <li class="main_header_departments_li_ul_li"><a title="" href="#">Categoria <?= "{$i}/{$ii}"; ?></a></li>
+                            <?php
+                        }
+                        ?>
+                    </ul>
+                </li>
+                <?php
+            }
+            ?>
+        </ul>
+    </div>
+</header>
+
+<footer class="main_footer">
+
+</footer>
+
+<script src="Assets/jquery.js"></script>
+<script src="Assets/scripts.js"></script>
 </body>
 </html>
