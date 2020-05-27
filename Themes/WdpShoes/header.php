b6f13ff4f07e7ecba3688e2334f29db3b77fc0a2
João <joaopedrolima.s@hotmail.com>
2020-05-27 07:45:44 -0300

    Desenvolvendo carrinho de compras, acesso do usuário e modal de login

--- Themes/WdpShoes/header.php
+++ Themes/WdpShoes/header.php
@@ -1,36 +1,29 @@
 <header class="main_header">
     <div class="container">
         <div class="main_header_nav">
-
-            <!--            NAV_LOGO-->
             <div class="main_header_nav_logo">
                 <a href="<?= $configBase; ?>" title="WdpShoes | Home">
                     <img alt="" title="" src="<?= $configThemeLink; ?>/Images/wdpshoes_logo_white.png"/>
                 </a>
             </div>
-
-            <!--            SEARCH NAV-->
             <div class="main_header_nav_search">
                 <form action="" method="post" class="radius">
                     <input type="text" name="s" placeholder="Pesquisar na WdpShoes:"/>
                     <button class="icon-search icon-notext transition"></button>
                 </form>
             </div>
-
-            <!--            MENU USER E CARRINHO-->
             <div class="main_header_nav_menu">
-                <a href="#" class="icon-cart icon-notext transition main_header_nav_menu_cart"><span>3</span></a>
+                <a href="<?= $configBase; ?>/checkout" class="icon-cart icon-notext transition main_header_nav_menu_cart"><span>3</span></a>
                 <div class="main_header_nav_menu_user">
-                    <a href="<?= "{$configBase}/conta"; ?>" title="" class="icon-user main_header_nav_menu_user_a radius transition">Minha Conta</a>
+                    <a href="#" title="" class="icon-user main_header_nav_menu_user_a radius transition jtheme_login">Login</a>
                     <nav class="radius">
-                        <a href="<?= "{$configBase}/conta/pedidos"; ?>" title="">Meus pedidos</a>
+                        <a href="<?= "{$configBase}/conta"; ?>" title="">Meus pedidos</a>
                         <a href="<?= "{$configBase}/conta/cadastro"; ?>" title="">Meus dados</a>
                         <a href="<?= "{$configBase}/conta/enderecos"; ?>" title="">Meus endereços</a>
-                        <a href="<?= "{$configBase}/conta/sair"; ?>" title="">Sair</a>
+                        <a href="<?= "{$configBase}"; ?>" title="">Sair</a>
                     </nav>
                 </div>
             </div>
-
         </div>
 
         <ul class="main_header_departments">
@@ -42,7 +35,7 @@
                         <?php
                         for ($ii = 1; $ii < 6; $ii++) {
                             ?>
-                            <li class="main_header_departments_li_ul_li"><a title="" href="<?= "{$configBase}/categoria"; ?>">Categoria <?= "{$i}/{$ii}"; ?></a></li>
+                            <li class="main_header_departments_li_ul_li"><a title="" href="<?= $configBase; ?>/categoria">Categoria <?= "{$i}/{$ii}"; ?></a></li>
                             <?php
                         }
                         ?>
